5e2c2bf936a1ac41ceefa0f76ec4db5e5facacdf
karantos <jan.repar@outlook.com>
2023-07-30 17:02:25 +0200

    updated index to show quote and table of quotes

--- website/views.py
+++ website/views.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+import urllib.request, json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        pass
+
+    else:
+        url = "https://stoic-quotes.com/api/quotes?nume=10"
+        # get response data (stoic quote from above url)
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        # convert json data to python dict
+        dict = json.loads(data)
+
+        quote = dict.pop()
+
+        return render_template('index.html', results = dict, quote = quote)
 
 @views.route('/favourites')
 @login_required
